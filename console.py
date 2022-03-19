536c133450f99b334cd18e003bb571d161f9f71f
CordiiT14 <cordelia.toennies@live.com>
2022-03-19 14:34:49 +0000

    booking repository delete all and save function written and tested

--- console.py
+++ console.py
@@ -9,6 +9,7 @@ import repositories.customer_repository as customer_repository
 
 event_repository.delete_all()
 customer_repository.delete_all()
+bookings_repository.delete_all()
 
 event_1 = Event("Loud Poets", "11 April 2022", "18:30", "Spoken Word", "Showcasing the top spoken word talent from Scotland and the UK! From laugh-out-loud funny, through the wonderfully surreal, to the thoughtful and emotional. Loud Poets continue to prove that Spoken Word has something for everyone.")
 
@@ -21,3 +22,9 @@ customer_1 = Customer("Sabrina", "Morales", "s.morales@emails.uk", "Customer is 
 customer_repository.save(customer_1)
 
 print(customer_1.__dict__)
+
+booking_1 = Booking(event_1, customer_1)
+
+bookings_repository.save(booking_1)
+
+print(booking_1.__dict__)
---
+++ repositories/bookings_repository.py
@@ -0,0 +1,16 @@
+from db.run_sql import run_sql
+from models.booking import Booking
+from models.customer import Customer
+from models.event import Event
+
+def delete_all():
+    sql = "DELETE FROM bookings"
+    run_sql(sql)
+    return
+
+def save(booking):
+    sql = "INSERT INTO bookings (event_id, customer_id) VALUES (%s, %s) RETURNING id"
+    values = [booking.event.id, booking.customer.id]
+    results = run_sql(sql, values)
+    booking.id = results[0]['id']
+    return booking 
