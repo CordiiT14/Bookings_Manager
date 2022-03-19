b269ffe0dceaa95f7b9d0f122c4a1d79a34eb604
CordiiT14 <cordelia.toennies@live.com>
2022-03-19 14:18:49 +0000

    Changed customer.notes = None. Customer notes not mandatory but optional field

--- models/customer.py
+++ models/customer.py
@@ -1,6 +1,6 @@
 class Customer:
 
-    def __init__(self, first_name, last_name, email, notes, id = None):
+    def __init__(self, first_name, last_name, email, notes = None, id = None):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
