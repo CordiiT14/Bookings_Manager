3537278dcca5d6725da7723edbe77fd6bac6d811
CordiiT14 <cordelia.toennies@live.com>
2022-03-19 12:53:18 +0000

    succesfully created db bookings_manager and added tables

--- db/bookings_manager.sql
+++ db/bookings_manager.sql
@@ -9,7 +9,7 @@ CREATE TABLE events (
     time VARCHAR(255),
     event_type VARCHAR(255),
     description TEXT
-)
+);
 
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
@@ -17,10 +17,10 @@ CREATE TABLE customers (
     last_name VARCHAR(255),
     email VARCHAR(255),
     notes TEXT
-)
+);
 
 CREATE TABLE bookings (
     id SERIAL PRIMARY KEY,
     event_id INT REFERENCES events(id) ON DELETE CASCADE,
     customer_id INT REFERENCES customers(id) ON DELETE CASCADE
-)
+);
