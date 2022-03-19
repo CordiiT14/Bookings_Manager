6b523658b99b638ec282bcca1fa763bffc3fc4b9
CordiiT14 <cordelia.toennies@live.com>
2022-03-19 12:50:29 +0000

    created db tables for events, customers, bookings

---
+++ db/bookings_manager.sql
@@ -0,0 +1,26 @@
+DROP TABLE IF EXISTS bookings;
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS events;
+
+CREATE TABLE events (
+    id SERIAL PRIMARY KEY,
+    event_title VARCHAR(255),
+    date VARCHAR(255),
+    time VARCHAR(255),
+    event_type VARCHAR(255),
+    description TEXT
+)
+
+CREATE TABLE customers (
+    id SERIAL PRIMARY KEY,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    email VARCHAR(255),
+    notes TEXT
+)
+
+CREATE TABLE bookings (
+    id SERIAL PRIMARY KEY,
+    event_id INT REFERENCES events(id) ON DELETE CASCADE,
+    customer_id INT REFERENCES customers(id) ON DELETE CASCADE
+)
