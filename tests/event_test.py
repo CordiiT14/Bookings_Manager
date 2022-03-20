a8531e5d1fca8940570b5a8914c236b89dd7d32c
CordiiT14 <cordelia.toennies@live.com>
2022-03-20 12:16:26 +0000

    Ran tests on customer attributes and added one test for event id

--- tests/event_test.py
+++ tests/event_test.py
@@ -1,3 +1,4 @@
+import string
 import unittest
 
 from models.event import Event
@@ -9,3 +10,21 @@ class TestEvent(unittest.TestCase):
 
     def test_event_name(self):
         self.assertEqual("Loud Poets", self.event_1.event_title)
+    
+    def test_event_date(self):
+        self.assertEqual("11 April 2022", self.event_1.date)
+
+    def test_event_time(self):
+        self.assertEqual("18:30", self.event_1.time)
+
+    def test_event_type(self):
+        self.assertEqual("Spoken Word", self.event_1.event_type)
+
+    def test_event_description_exists(self):
+        self.assertNotEqual( None , self.event_1.description)
+    
+    def test_event_description(self):
+        self.assertEqual( "Showcasing the top spoken word talent from Scotland and the UK! From laugh-out-loud funny, through the wonderfully surreal, to the thoughtful and emotional. Loud Poets continue to prove that Spoken Word has something for everyone." , self.event_1.description)
+
+    def test_event_id(self):
+        self.assertEqual( None, self.event_1.id)
