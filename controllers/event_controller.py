6e8af0b865fc803af1f6c58cdc7f1620f00b0006
CordiiT14 <cordelia.toennies@live.com>
2022-03-21 09:32:41 +0000

    outlined needed routes in events_controller

--- controllers/event_controller.py
+++ controllers/event_controller.py
@@ -4,3 +4,19 @@ from app import app
 from models.event import Event
 
 events_blueprint = Blueprint("events", __name__)
+
+@events_blueprint.route('/events')
+def view_all_events():
+    pass
+
+@events_blueprint.route('/events/new')
+def new_events():
+    pass
+
+@events_blueprint.route('/events/<id>')
+def view_event_details():
+    pass
+
+@events_blueprint.route('/events/edit')
+def edit_event():
+    pass
