fdd5052414939fb5ceba954898959939add35cc9
CordiiT14 <cordelia.toennies@live.com>
2022-03-21 18:11:43 +0000

    succesfully booking customer onto event. not redirecting as intended

--- controllers/bookings_controller.py
+++ controllers/bookings_controller.py
@@ -1,7 +1,7 @@
 from flask import render_template, redirect, request
 from flask import Blueprint
 from models.booking import Booking
-from repositories.bookings_repository import customer
+import repositories.bookings_repository as booking_repository
 
 import repositories.customer_repository as customer_repository
 import repositories.event_repository as event_repository
@@ -13,3 +13,13 @@ def book_event(id):
     event = event_repository.select(id)
     customers = customer_repository.select_all()
     return render_template('/bookings/book_event.html', title = 'New Booking', event = event, all_customers = customers)
+
+@bookings_blueprint.route('/bookings/new', methods=['POST'])
+def add_booking():
+    event_id = request.form['event_id']
+    customer_id = request.form['customer_id']
+    event = event_repository.select(event_id)
+    customer = customer_repository.select(customer_id)
+    booking = Booking(event, customer)
+    booking_repository.save(booking)
+    return redirect('/events/event_id')
