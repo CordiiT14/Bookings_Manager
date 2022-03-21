f3bf5f49f080dcad96e4de52deb6c201781d7abd
CordiiT14 <cordelia.toennies@live.com>
2022-03-21 14:11:44 +0000

    display all events on webpage working

--- controllers/event_controller.py
+++ controllers/event_controller.py
@@ -1,20 +1,23 @@
 from flask import render_template, redirect, request
 from flask import Blueprint
 from models.event import Event
+import repositories.event_repository as event_repository
 
 events_blueprint = Blueprint("events", __name__)
 
 @events_blueprint.route('/events')
 def view_all_events():
-    pass
+    events = event_repository.select_all()
+    return render_template('/events/index.html', title = 'All Events', all_events = events)
 
 @events_blueprint.route('/events/new')
 def new_events():
     pass
 
 @events_blueprint.route('/events/<id>')
-def view_event_details():
-    pass
+def view_event_details(id):
+    event = event_repository.select(id)
+    return render_template('/events/view.html', title=event.event_title, event = event)
 
 @events_blueprint.route('/events/edit')
 def edit_event():
