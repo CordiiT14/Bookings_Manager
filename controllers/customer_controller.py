da3df5066f65fbb5c45fd2e088502fad0e1cd257
CordiiT14 <cordelia.toennies@live.com>
2022-03-21 09:38:02 +0000

    added outline of customer_controller paths. Added index files for events and customer paths.

--- controllers/customer_controller.py
+++ controllers/customer_controller.py
@@ -4,3 +4,19 @@ from app import app
 from models.customer import Customer
 
 customers_blueprint = Blueprint("customers", __name__)
+
+@customers_blueprint.route('/customers')
+def view_all_customer():
+    pass
+
+@customers_blueprint.route('/customers/new')
+def new_customer():
+    pass
+
+@customers_blueprint.route('/customers/<id>')
+def view_event_details():
+    pass
+
+@customers_blueprint.route('/customers/edit')
+def edit_event():
+    pass
